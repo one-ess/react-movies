b172333af741503f1b9576d9aa51bf2d6eb26d4b
TArtur <onese7enzzz@gmail.com>
2023-01-26 22:50:04 +0600

    added no poster option

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -2,7 +2,8 @@ const Movie = (props) => {
   const { Title, Year, Type, Poster } = props;
   return (
     <li className="movie__item">
-      <img className="movie__img" src={Poster} alt={Title} />
+      {Poster === "N/A" ? <div>No Image</div> : <img className="movie__img" src={Poster} alt={Title} />}
+
       <h3 className="movie__title">{Title}</h3>
       <p className="movie__year">{Year}</p>
       <p className="movie__type">{Type}</p>
