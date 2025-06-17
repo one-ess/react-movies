7af79b52c53648e1d32db02a32b10e5dcb39d4e4
TArthur <onese7enzzz@gmail.com>
2025-06-17 19:00:13 +0500

    modified actions

--- src/context/reducer.js
+++ src/context/reducer.js
@@ -1,34 +1,47 @@
 const reducer = (state, action) => {
   switch (action.type) {
-    case "SET_MOVIES":
-      return {
-        ...state,
-        movies: action.payload,
-      };
     case "SET_LOADING":
       return {
         ...state,
         loading: action.payload,
       };
-    case "SET_TOTAL_PAGES":
+    case "CHANGE_TEXT": {
+      const trimmedValue = action.payload.target.value.trim();
+
+      if (trimmedValue) {
+        return {
+          ...state,
+          searchText: trimmedValue,
+          currentPage: 1,
+        };
+      } else {
+        return state;
+      }
+    }
+    case "CHANGE_RADIO":
       return {
         ...state,
-        totalPages: action.payload,
+        radioValue: action.payload.target.value,
+        currentPage: 1,
       };
-    case "SET_SEARCH_TEXT":
-      return {
-        ...state,
-        searchText: action.payload,
-      };
-    case "SET_CURRENT_PAGE":
+    case "HANDLE_OMDB_RESPONSE":
+      if (action.payload.Response === "False") {
+        return {
+          ...state,
+          movies: [],
+          totalPages: 1,
+        };
+      } else {
+        return {
+          ...state,
+          movies: action.payload.Search,
+          totalPages: Math.ceil(Number(action.payload.totalResults) / 10),
+        };
+      }
+    case "GO_TO_PAGE":
       return {
         ...state,
         currentPage: action.payload,
-      };
-    case "SET_RADIO_VALUE":
-      return {
-        ...state,
-        radioValue: action.payload,
       };
     default:
       return state;
